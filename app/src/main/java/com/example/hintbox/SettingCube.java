f123750dbc7f2d76acb576817f6484980039d28f
bugemdai <37519790+bugemdai@users.noreply.github.com>
2025-03-13 10:51:34 +0300

    Update SettingCube.java

--- app/src/main/java/com/example/hintbox/SettingCube.java
+++ app/src/main/java/com/example/hintbox/SettingCube.java
@@ -30,6 +30,7 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class SettingCube extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -42,6 +43,14 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
     private ColorHSV yellowHsv, orangeHsv, greenHsv, blueHsv, whiteHsv, redHsv, settingHsv;
     private Scalar scalarLowSetting, scalarHightSetting;
     private Button buttonYellow, buttonWhite, buttonOrange, buttonBlue, buttonRed, buttonSave;
+
+    private static final String PREF_COLOR_SETTING = "ColorSetting";
+    private static final String KEY_HL = "Hl";
+    private static final String KEY_HH = "Hh";
+    private static final String KEY_SL = "Sl";
+    private static final String KEY_SH = "Sh";
+    private static final String KEY_VL = "Vl";
+    private static final String KEY_VH = "Vh";
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -67,34 +76,58 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_cube);
 
+        initializeCameraView();
+        setupBottomNavigationView();
+        initializeColorSettings();
+        setupButtons();
+        setupRangeSliders();
+    }
+
+    /**
+     * Initializes the camera view and sets its properties.
+     */
+    private void initializeCameraView() {
         mOpenCvCameraView = (JavaCameraView) findViewById(R.id.myCameraView);
         mOpenCvCameraView.setMaxFrameSize(640, 800);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
+    /**
+     * Sets up the bottom navigation view and its item selection listener.
+     */
+    private void setupBottomNavigationView() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.setting);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+    }
 
-                switch (id) {
-                    case R.id.solver:
-                        startActivity(new Intent(getApplicationContext(), Solver.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.cube:
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.setting:
-                        return true;
-                }
-                return false;
-            }
-        });
+    /**
+     * Handles navigation item selection.
+     * @param item The selected menu item.
+     * @return true if the item selection is handled.
+     */
+    private boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.solver:
+                startActivity(new Intent(getApplicationContext(), Solver.class));
+                overridePendingTransition(0, 0);
+                return true;
+            case R.id.cube:
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            case R.id.setting:
+                return true;
+        }
+        return false;
+    }
 
+    /**
+     * Initializes color settings for different colors.
+     */
+    private void initializeColorSettings() {
         yellowHsv = setSettingHSV("Y", "yellow");
         greenHsv = setSettingHSV("G", "green");
         orangeHsv = setSettingHSV("O", "orange");
@@ -103,147 +136,114 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
         blueHsv = setSettingHSV("B", "blue");
 
         settingHsv = yellowHsv;
-        scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-        scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
+        updateScalars();
+    }
 
-        FrameLayout frameLayout = findViewById(R.id.frame_layout);
-
+    /**
+     * Sets up button click listeners for color selection and saving.
+     */
+    private void setupButtons() {
         buttonYellow = findViewById(R.id.yellow);
-        buttonYellow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = yellowHsv;
-                setProgress(yellowHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonYellow.setOnClickListener(v -> onColorButtonClicked(yellowHsv));
 
         buttonBlue = findViewById(R.id.blue);
-        buttonBlue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = blueHsv;
-                setProgress(blueHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonBlue.setOnClickListener(v -> onColorButtonClicked(blueHsv));
 
         Button buttonGreen = findViewById(R.id.green);
-        buttonGreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = greenHsv;
-                setProgress(greenHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonGreen.setOnClickListener(v -> onColorButtonClicked(greenHsv));
 
         buttonOrange = findViewById(R.id.orange);
-        buttonOrange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = orangeHsv;
-                setProgress(orangeHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonOrange.setOnClickListener(v -> onColorButtonClicked(orangeHsv));
 
         buttonRed = findViewById(R.id.red);
-        buttonRed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = redHsv;
-                setProgress(redHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonRed.setOnClickListener(v -> onColorButtonClicked(redHsv));
 
         buttonWhite = findViewById(R.id.white);
-        buttonWhite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                settingHsv = whiteHsv;
-                setProgress(whiteHsv);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
-            }
-        });
+        buttonWhite.setOnClickListener(v -> onColorButtonClicked(whiteHsv));
 
         buttonSave = findViewById(R.id.save);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                colorToSetting(yellowHsv, "Y");
-                colorToSetting(greenHsv,  "G");
-                colorToSetting(orangeHsv, "O");
-                colorToSetting(whiteHsv,  "W");
-                colorToSetting(redHsv,    "R");
-                colorToSetting(blueHsv,   "B");
-            }
-        });
+        buttonSave.setOnClickListener(v -> saveColorSettings());
+    }
 
+    /**
+     * Handles color button click events.
+     * @param colorHsv The selected color's HSV settings.
+     */
+    private void onColorButtonClicked(ColorHSV colorHsv) {
+        settingHsv = colorHsv;
+        setProgress(colorHsv);
+        updateScalars();
+    }
+
+    /**
+     * Sets up range sliders for HSV values.
+     */
+    private void setupRangeSliders() {
         rangeH = findViewById(R.id.rangeH);
         rangeH.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(@NonNull RangeSlider slider) {
-                List<Float> list = rangeH.getValues();
-                float min = list.get(0);
-                float max = list.get(1);
-                settingHsv.setLowH((int) min);
-                settingHsv.setHightH((int) max);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
+                updateHSVFromSlider(rangeH, settingHsv::setLowH, settingHsv::setHightH);
             }
 
             @Override
-            public void onStopTrackingTouch(@NonNull RangeSlider slider) {
-
-            }
+            public void onStopTrackingTouch(@NonNull RangeSlider slider) {}
         });
 
         rangeS = findViewById(R.id.rangeS);
         rangeS.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(@NonNull RangeSlider slider) {
-                List<Float> list = rangeS.getValues();
-                float min = list.get(0);
-                float max = list.get(1);
-                settingHsv.setLowS((int) min);
-                settingHsv.setHightS((int) max);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
+                updateHSVFromSlider(rangeS, settingHsv::setLowS, settingHsv::setHightS);
             }
 
             @Override
-            public void onStopTrackingTouch(@NonNull RangeSlider slider) {
-
-            }
+            public void onStopTrackingTouch(@NonNull RangeSlider slider) {}
         });
 
         rangeV = findViewById(R.id.rangeV);
         rangeV.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(@NonNull RangeSlider slider) {
-                List<Float> list = rangeV.getValues();
-                float min = list.get(0);
-                float max = list.get(1);
-                settingHsv.setLowV((int) min);
-                settingHsv.setHightV((int) max);
-                scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
-                scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
+                updateHSVFromSlider(rangeV, settingHsv::setLowV, settingHsv::setHightV);
             }
 
             @Override
-            public void onStopTrackingTouch(@NonNull RangeSlider slider) {
-
-            }
+            public void onStopTrackingTouch(@NonNull RangeSlider slider) {}
         });
+    }
 
+    /**
+     * Updates HSV values from the slider.
+     * @param slider The RangeSlider for HSV values.
+     * @param setLow Consumer to set the low value.
+     * @param setHigh Consumer to set the high value.
+     */
+    private void updateHSVFromSlider(RangeSlider slider, Consumer<Integer> setLow, Consumer<Integer> setHigh) {
+        List<Float> values = slider.getValues();
+        setLow.accept(values.get(0).intValue());
+        setHigh.accept(values.get(1).intValue());
+        updateScalars();
+    }
+
+    /**
+     * Updates the scalar values for the current color setting.
+     */
+    private void updateScalars() {
+        scalarLowSetting = new Scalar(settingHsv.getLowH(), settingHsv.getLowS(), settingHsv.getLowV());
+        scalarHightSetting = new Scalar(settingHsv.getHightH(), settingHsv.getHightS(), settingHsv.getHightV());
+    }
+
+    /**
+     * Saves the current color settings to SharedPreferences.
+     */
+    private void saveColorSettings() {
+        colorToSetting(yellowHsv, "Y");
+        colorToSetting(greenHsv, "G");
+        colorToSetting(orangeHsv, "O");
+        colorToSetting(whiteHsv, "W");
+        colorToSetting(redHsv, "R");
+        colorToSetting(blueHsv, "B");
     }
 
     @Override
@@ -265,6 +265,7 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
         }
     }
 
+    @Override
     public void onDestroy() {
         super.onDestroy();
         if (mOpenCvCameraView != null)
@@ -285,35 +286,48 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
         mResult.release();
     }
 
-    public void setProgress (ColorHSV colorHSV) {
+    /**
+     * Sets the progress of the sliders based on the given color HSV values.
+     * @param colorHSV The color HSV values to set.
+     */
+    public void setProgress(ColorHSV colorHSV) {
         rangeH.setValues((float) colorHSV.getLowH(), (float) colorHSV.getHightH());
         rangeS.setValues((float) colorHSV.getLowS(), (float) colorHSV.getHightS());
         rangeV.setValues((float) colorHSV.getLowV(), (float) colorHSV.getHightV());
     }
 
-    public ColorHSV setSettingHSV (String color, String name) {
-        ColorHSV temp = null;
-        SharedPreferences setting = getApplicationContext().getSharedPreferences("ColorSetting", 0);
-        temp = new ColorHSV(name,
-                setting.getInt(String.format(color + "Hl"), 0),
-                setting.getInt(String.format(color + "Hh"), 0),
-                setting.getInt(String.format(color + "Sl"), 0),
-                setting.getInt(String.format(color + "Sh"), 0),
-                setting.getInt(String.format(color + "Vl"), 0),
-                setting.getInt(String.format(color + "Vh"), 0));
-        return temp;
+    /**
+     * Retrieves and sets the HSV settings for a given color from SharedPreferences.
+     * @param color The color code.
+     * @param name The name of the color.
+     * @return The ColorHSV object with the settings.
+     */
+    public ColorHSV setSettingHSV(String color, String name) {
+        SharedPreferences setting = getApplicationContext().getSharedPreferences(PREF_COLOR_SETTING, 0);
+        return new ColorHSV(name,
+                setting.getInt(String.format(color + KEY_HL), 0),
+                setting.getInt(String.format(color + KEY_HH), 0),
+                setting.getInt(String.format(color + KEY_SL), 0),
+                setting.getInt(String.format(color + KEY_SH), 0),
+                setting.getInt(String.format(color + KEY_VL), 0),
+                setting.getInt(String.format(color + KEY_VH), 0));
     }
 
-    public void colorToSetting (ColorHSV colorHSV, String color) {
-        SharedPreferences settings = getApplicationContext().getSharedPreferences("ColorSetting", 0);
+    /**
+     * Saves the HSV settings of a color to SharedPreferences.
+     * @param colorHSV The ColorHSV object containing the settings.
+     * @param color The color code.
+     */
+    public void colorToSetting(ColorHSV colorHSV, String color) {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(PREF_COLOR_SETTING, 0);
         SharedPreferences.Editor editor = settings.edit();
 
-        editor.putInt(String.format(color + "Hl"), colorHSV.getLowH());
-        editor.putInt(String.format(color + "Hh"), colorHSV.getHightH());
-        editor.putInt(String.format(color + "Sl"), colorHSV.getLowS());
-        editor.putInt(String.format(color + "Sh"), colorHSV.getHightS());
-        editor.putInt(String.format(color + "Vl"), colorHSV.getLowV());
-        editor.putInt(String.format(color + "Vh"), colorHSV.getHightV());
+        editor.putInt(String.format(color + KEY_HL), colorHSV.getLowH());
+        editor.putInt(String.format(color + KEY_HH), colorHSV.getHightH());
+        editor.putInt(String.format(color + KEY_SL), colorHSV.getLowS());
+        editor.putInt(String.format(color + KEY_SH), colorHSV.getHightS());
+        editor.putInt(String.format(color + KEY_VL), colorHSV.getLowV());
+        editor.putInt(String.format(color + KEY_VH), colorHSV.getHightV());
 
         editor.apply();
     }
@@ -322,14 +336,11 @@ public class SettingCube extends AppCompatActivity implements CameraBridgeViewBa
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mFrame = inputFrame.rgba();
         Core.transpose(mFrame, mFrame);
-        Imgproc.resize(mFrame, mFrame, mFrame.size(), 0,0, 0);
-        Core.flip(mFrame, mFrame, 1 );
+        Imgproc.resize(mFrame, mFrame, mFrame.size(), 0, 0, 0);
+        Core.flip(mFrame, mFrame, 1);
 
         Imgproc.cvtColor(mFrame, mFrame, Imgproc.COLOR_BGR2HSV_FULL);
         Core.inRange(mFrame, scalarLowSetting, scalarHightSetting, mMask);
-
-//        Imgproc.cvtColor(mFrame, mFrame, Imgproc.COLOR_HSV2BGR_FULL);
-//        Core.bitwise_and(mFrame, mFrame, mResult, mMask);
 
         return mMask;
     }
